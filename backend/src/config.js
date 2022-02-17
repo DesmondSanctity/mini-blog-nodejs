aff2c7bc052703f667ef7196bcdd7ff0c1302813
Desmond Obisi <51109125+DesmondSanctity@users.noreply.github.com>
2022-02-17 09:14:21 +0100

    update port listening

--- backend/src/config.js
+++ backend/src/config.js
@@ -1,6 +1,6 @@
 import path from 'path';
 
 export default {
-    port: process.env.PORT || 5000,
+    port: process.env.PORT || 8080,
     dbPath: process.env.DB_PATH || path.join(__dirname, '..', 'db.sqlite')
 };
